29be99263ace27029ac627178b30f92249a1dc30
awt-256 <68395886+awt-256@users.noreply.github.com>
2023-05-16 15:07:27 -0400

    push views

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
-// app.set("views", __dirname + "/views");
-// app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//     res.render("index");
-// });
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/app/gallery", (req, res) => {
+    res.render("gallery");
+});
+app.get("/app/profile", (req, res) => {
+    res.render("profile");
+});
+app.get("/app/transactions", (req, res) => {
+    res.render("gallery");
+});
 
 app.listen(PORT, () => {
     console.log(`App server listening on ${PORT}. (http://localhost:${PORT})`);
