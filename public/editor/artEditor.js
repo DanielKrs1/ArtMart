a94ba3e1b7cfff54c6d49e46f3c494171782e99b
DanielKrs1 <krstndaniel24@gmail.com>
2023-05-19 15:16:20 -0400

    commit

--- public/editor/artEditor.js
+++ public/editor/artEditor.js
@@ -1,10 +1,13 @@
+const CANVAS_SIZE = 400;
+const RESOLUTION = 16;
+const PIXEL_SIZE = CANVAS_SIZE / RESOLUTION;
+
 function setup() {
-    canvasContainer = document.getElementById("canvasContainer");
-    canvas = createCanvas(400, 400);
+    let canvasContainer = document.getElementById("canvasContainer");
+    let canvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE);
     canvas.parent(canvasContainer);
 }
   
 function draw() {
-    background(200);
-    ellipse(mouseX, mouseY, 40, 40);
+    
 }
