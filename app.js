f64e4b777676a0a0aaf2ce3c3badd980aa53a67a
awt-256 <68395886+awt-256@users.noreply.github.com>
2023-05-19 10:40:45 -0400

    frontends

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+// remove on oauth
+app.get("/login", (req, res) => res.redirect("/app/profile"))
+
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -19,10 +22,16 @@ app.get("/app/gallery", (req, res) => {
     res.render("gallery");
 });
 app.get("/app/profile", (req, res) => {
-    res.render("profile");
+    res.render("profile", {user: {name: "Test Account"}});
+});
+app.get("/app/editor", (req, res) => {
+    res.render("editor");
 });
 app.get("/app/transactions", (req, res) => {
-    res.render("gallery");
+    res.render("transactions", { transactions: [
+        { senderName: "Jony", receiverName: "Teddy", artName: "nyan cat [todo link]", timeStarted: new Date(10000000000).getTime()},
+        { senderName: "Charles", receiverName: "Ben", artName: "aonother cat [todo link]", timeStarted: new Date(10005000000).getTime()}
+    ]});
 });
 
 app.listen(PORT, () => {
---
+++ public/editor/artEditor.js
@@ -0,0 +1,7 @@
+function setup() {
+    createCanvas(16, 16);
+}
+  
+  function draw() {
+    background(220);
+  }
