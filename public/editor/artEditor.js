9057312233be1439e0551bfb6edc94db457c4b9c
DanielKrs1 <krstndaniel24@gmail.com>
2023-05-21 09:34:09 -0400

    more painting

--- public/editor/artEditor.js
+++ public/editor/artEditor.js
@@ -6,20 +6,26 @@ const COLORS = [
     [255, 0, 0],
     [0, 255, 0],
     [0, 0, 255],
-    [255, 255, 255]
+    [255, 255, 255],
 ]
+const TOOLS = {
+    brush : 0,
+    fill : 1
+}
 
 let grid = [];
 let selectedColorIndex;
+let selectedTool;
 
 function setup() {
     let canvasContainer = document.getElementById("canvasContainer");
     let canvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE);
     canvas.parent(canvasContainer);
 
+    setSelectedColorIndex(4);
+    setSelectedTool(TOOLS.brush);
     initializeGrid();
     createColorButtons();
-    setSelectedColorIndex(0);
     updateDraw();
 }
   
@@ -35,24 +41,53 @@ function updateDraw() {
             rect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
         }
     }
+
+    stroke(0);
+    strokeWeight(2);
+    let border = CANVAS_SIZE - 1;
+    line(0, 0, 0, border);
+    line(border, 0, border, border);
+    line(0, 0, border, 0);
+    line(0, border, border, border);
 }
 
 function draw() {
-    if (!mouseIsPressed)
-        return;
-
-    if (mouseX < 0 || mouseY < 0 || mouseX >= CANVAS_SIZE || mouseY >= CANVAS_SIZE)
+    if (selectedTool != TOOLS.brush || !mouseIsPressed || isMouseOffCanvas())
         return;
     
-    let x = floor(mouseX / PIXEL_SIZE);
-    let y = floor(mouseY / PIXEL_SIZE);
-    grid[x][y] = selectedColorIndex;
+    let xy = getMouseGridXY();
+    grid[xy[0]][xy[1]] = selectedColorIndex;
 
     updateDraw();
 }
 
+function mouseClicked() {
+    if (selectedTool != TOOLS.fill || isMouseOffCanvas())
+        return;
+    
+    let xy = getMouseGridXY();
+    paintWithFill(xy[0], xy[1]);    
+    updateDraw();
+}
+
+function isMouseOffCanvas() {
+    return mouseX < 0 || mouseY < 0 || mouseX >= CANVAS_SIZE || mouseY >= CANVAS_SIZE;
+}
+
+function getMouseGridXY() {
+    return [floor(mouseX / PIXEL_SIZE), floor(mouseY / PIXEL_SIZE)];
+}
+
+function paintWithFill(startX, startY) {
+    throw new Error("Implement fill tool!");
+}
+
 function setSelectedColorIndex(i) {
     selectedColorIndex = i;
+}
+
+function setSelectedTool(tool) {
+    selectedTool = tool;
 }
 
 function initializeGrid() {
@@ -60,7 +95,7 @@ function initializeGrid() {
         let column = [];
 
         for (let y = 0; y < RESOLUTION; y++)
-            column.push(0);
+            column.push(selectedColorIndex);
 
         grid.push(column);        
     }
@@ -76,6 +111,9 @@ function createColorButtons() {
         button.style("color", hex);
         button.mouseClicked(() => setSelectedColorIndex(((x) => x)(i)));
     }
+
+    createButton("Brush").mouseClicked(() => setSelectedTool(TOOLS.brush));
+    createButton("Fill").mouseClicked(() => setSelectedTool(TOOLS.fill));
 }
 
 function colorToHex(color) {
